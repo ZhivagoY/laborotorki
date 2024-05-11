7fdd92f610a2344742e70c96253975252fea9d56
ZhivagoY <143877343+ZhivagoY@users.noreply.github.com>
2024-05-11 16:13:00 +0200

    Добавлены файлы для Лабы 1

---
+++ Семестр 2/func.hpp
@@ -0,0 +1,6 @@
+#pragma once
+#include <string>
+
+namespace al {
+	void writeTimeToFile(const std::string& filename);
+}
---
+++ Семестр 2/main.cpp
@@ -0,0 +1,17 @@
+﻿#include <iostream>
+#include "func.hpp"
+
+
+using namespace std;
+
+int main()
+{
+    setlocale(LC_ALL, "rus");
+
+    std::string filename = "input.txt";
+    al::writeTimeToFile(filename);
+
+    std::cout << "Информация записана в файл " << filename << std::endl;
+
+    return 0;
+}
